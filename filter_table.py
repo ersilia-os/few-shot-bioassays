798fa640fb43dbcc96e1ae866af9ab948fafeabb
Phevos Paschalidis <phevos.paschalidis@gmail.com>
2024-03-30 13:46:15 -0400

    Saving histograms

--- filter_table.py
+++ filter_table.py
@@ -15,7 +15,7 @@ plt.hist(x, range = [0, 75], bins=15, weights=np.ones(len(x)) / len(x))
 plt.xlabel('Number of compounds')
 plt.ylabel('Percentage')
 plt.title('Compounds per assay')
-plt.show()
+plt.savefig('compounds_per_assay.png')
 # Seems that ~60% of the assays have fewer than 5 compounds
 # We filter those out
 
@@ -25,11 +25,12 @@ print('Post Filtering, # of unique assays', len(filtered_df['assay_id'].unique()
 print('Post Filtering, # of unique compounds', len(filtered_df))
 
 # # Plotting the histogram
+plt.clf()
 plt.hist(filtered_df['pchembl_value'])
 plt.xlabel('pchembl_value')
 plt.ylabel('Frequency')
 plt.title('Histogram of pchembl_value')
-plt.show()
+plt.savefig('pchebml_value_histogram.png')
 
 # For each line of bioassay_table_filtered.csv, 
 # if pchembl_value > cutoff then active = true, otherwise active = false
